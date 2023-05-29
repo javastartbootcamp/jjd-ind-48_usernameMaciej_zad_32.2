257c37068a6cedd4672ceabafa71f995a982bfe2
usernameMaciej <maciej.przystarz@protonmail.com>
2023-05-29 11:16:39 +0200

    rozwiazanie

--- src/main/java/pl/javastart/jpaoptimalization/countrylanguage/CountryLanguageRepository.java
+++ src/main/java/pl/javastart/jpaoptimalization/countrylanguage/CountryLanguageRepository.java
@@ -1,6 +1,12 @@
 package pl.javastart.jpaoptimalization.countrylanguage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CountryLanguageRepository extends JpaRepository<CountryLanguage, CountryLanguageKey> {
+
+    @Query("SELECT DISTINCT c FROM CountryLanguage c ORDER BY c.language")
+    List<CountryLanguage> findAllCountriesWithLanguages();
 }
